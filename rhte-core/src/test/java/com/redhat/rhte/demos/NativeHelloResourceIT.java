fd21511cf504a42c3c6dd559027e85a7ce48e7f8
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-08-25 06:41:49 -0400

    initial project

---
+++ rhte-core/src/test/java/com/redhat/rhte/demos/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.redhat.rhte.demos;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
